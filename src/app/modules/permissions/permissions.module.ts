a525753b7f0e631f28c8b43faa5707414cd2bd08
Ryan Pelham <pelhamrj@gmail.com>
2021-05-15 22:41:44 -0400

    Added permissions module

---
+++ src/app/modules/permissions/permissions.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PermissionsRoutingModule } from './permissions-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, PermissionsRoutingModule],
+})
+export class PermissionsModule {}
