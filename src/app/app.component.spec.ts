d489adebcb047acf3d354e771da376bcd684a0c3
Ryan Pelham <pelhamrj@gmail.com>
2021-05-15 21:29:06 -0400

    Updated prettier config

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,12 +13,14 @@ describe('AppComponent', () => {
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
+
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'tax-suite-ui'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
+
     expect(app.title).toEqual('tax-suite-ui');
   });
 
@@ -26,6 +28,7 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
+
     expect(compiled.querySelector('.content span').textContent).toContain('tax-suite-ui app is running!');
   });
 });
