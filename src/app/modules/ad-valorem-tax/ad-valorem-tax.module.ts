3eb52eaecd3831ef9953cac0ac36a0b995592500
Ryan Pelham <pelhamrj@gmail.com>
2021-05-15 23:01:50 -0400

    Added search pages

--- src/app/modules/ad-valorem-tax/ad-valorem-tax.module.ts
+++ src/app/modules/ad-valorem-tax/ad-valorem-tax.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdValoremTaxRoutingModule } from './ad-valorem-tax-routing.module';
+import { SearchComponent } from './pages/search/search.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [SearchComponent],
   imports: [CommonModule, AdValoremTaxRoutingModule],
 })
 export class AdValoremTaxModule {}
--- src/app/modules/business-tax/business-tax.module.ts
+++ src/app/modules/business-tax/business-tax.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BusinessTaxRoutingModule } from './business-tax-routing.module';
+import { SearchComponent } from './pages/search/search.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [SearchComponent],
   imports: [CommonModule, BusinessTaxRoutingModule],
 })
 export class BusinessTaxModule {}
